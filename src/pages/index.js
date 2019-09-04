e6d88393e3c82f754a7a2cc470a99f9d444b22a7
Greg Knudsen <dev.artist15@gmail.com>
2019-09-04 15:47:35 -0400

    add global styles with @emotion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default () => (
   <Layout>
     <div>
-      <h2> Hello Frontend Masters!</h2>
+      <h1>Home</h1>
+      <p> Hello Frontend Masters!</p>
       <Link to="/about">Learn about me!</Link>
     </div>
   </Layout>
