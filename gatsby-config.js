aef3aa5d891099b29609bde14f7484a48a224178
Greg Knudsen <dev.artist15@gmail.com>
2019-09-04 19:45:48 -0400

    title coming back as undefined

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion']
-}
+  siteMetadata: {
+    title: 'Front End Masters Gatsby Workshop',
+    description: 'A site build together at FEM'
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet']
+};
