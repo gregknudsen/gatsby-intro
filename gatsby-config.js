a98b6723a31197bbf7fe4f7dd202234ce228683b
Greg Knudsen <gknudsen@duqlight.com>
2019-09-05 08:02:34 -0400

    add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion']
+  siteMetadata: {
+    title: 'Frontend Masters Gatsby Introduction',
+    description: 'Fun was had by all!'
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet']
 }
