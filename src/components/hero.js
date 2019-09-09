4151c0306d3204038ece4f4a3bb326c26612f349
Greg Knudsen <gknudsen@duqlight.com>
2019-09-09 10:08:05 -0400

    hero image now displayed

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Link, graphql, useStaticQuery } from 'gatsby';
-import BackgroundImage from 'gatsby-background-image'
+import BackgroundImage from 'gatsby-background-image';
 
 const ImageBackground = styled(BackgroundImage)`
   background-position: top 20% center;
@@ -18,16 +18,16 @@ const TextBox = styled('div')`
   flex-direction: column;
   height: 100%;
   justify-content: flex-end;
-  padding: 0 calc((100vh - 550px) / 2) 2rem;
+  padding: 0 calc((100vw - 550px) / 2) 2rem;
   width: 100%;
 
   h1 {
-    text-shadow: 1px 1pm 3px #eeddff66;
+    text-shadow: 1px 1px 3px #eeddff66;
     font-size: 2.25rem;
   }
 
   p, a {
-    color: #222,
+    color: #222;
     margin-top: 0;
   }
 
@@ -37,17 +37,19 @@ const TextBox = styled('div')`
 `
 
 const Hero = () => {
-  const { image } = useStaticQuery(graphql`
-    {
-      image: file(relativePath: { eq: "pittsburgh.jpq" }) {
+    const {
+      image
+    } = useStaticQuery(graphql `
+    query {
+      image: file(relativePath: { eq: "pittsburgh.jpg" }) {
         sharp: childImageSharp {
           fluid {
-            ...GatsbyImageSharpFluid
+            ...GatsbyImageSharpFluid_withWebp
           }
         }
       }
     }
-  `)
+  `);
   return (
     <ImageBackground Tag="section" fluid={image.sharp.fluid}>
       <TextBox>
