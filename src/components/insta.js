8375c41f144b78d609257160dae3123f3343410e
Greg Knudsen <gknudsen@duqlight.com>
2019-09-10 09:40:08 -0400

    connect to gatsbyjs Instagram account

--- src/components/insta.js
+++ src/components/insta.js
@@ -10,15 +10,45 @@ const Insta = () => {
   return (
     <>
       <h2>Instagram posts from @{username}</h2>
-      <div>
-        { instaPhotos.map(photo => {
+      <div
+        css={css`
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          margin: 1rem -0.5rem;
+        `}>
+        { instaPhotos.map(photo => (
           <a
+            key={photo.id}
             href={`https://instagram.com/p/${photo.id}`}
             css={css`
-              box-shadow: 1px 1px 3px #ddd;
+              box-shadow: 0;
+              display: block;
+              margin: 0.5rem;
+              max-width: calc(33% - 1rem);
+              width: 120px;
+              transition: 200ms box-shadow linear;
+
+              :focus,
+              :hover {
+                box-shadow: 0 2px 14px #22222244;
+                z-index: 10;
+              }
             `}
-            ></a>
-        })}
+            >
+              <Image
+                fluid={photo.fluid}
+                alt={photo.caption}
+                css={css`
+                  width: 100%;
+
+                  * {
+                    margin-top: 0;
+                  }
+                `}
+              />
+            </a>
+        ))}
       </div>
       <a href={`https://instgram.com/${username}`}>
         See more on Instagram &rarr;
