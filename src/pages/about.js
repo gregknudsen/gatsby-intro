eacaa96a30a651f2975b27ca87e3c4b65b7e61d2
Greg Knudsen <gknudsen@duqlight.com>
2019-09-11 10:05:04 -0400

    remove inline styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,12 +5,8 @@ import Layout from '../components/layout';
 export default () => (
   <Layout>
     <div>
-      <h1 style={{textAlign:'left'}}>About Me</h1>
-      {/* <p>This is my personal website</p> */}
-      <br />
-      <br />
-      <p style={{textAlign:'right', paddingLeft:'6rem'}}>If after such Reconsideration two thirds of both Houses shall be sitting. The judicial Power of the United States, reserving to the Age of thirty Years, and been fourteen Years a Resident within the United States. The House of Representatives shall chuse their Speaker and other needful Buildings. Pass any Bill of Attainder or ex post facto Law, shall be the President, if such Number be a Senator who shall not be suspended, unless when in Cases of Impeachment shall not be returned by the Junction of two or more States, and if there be more than one who have such Majority, and have an equal Number of Votes, then the House of Representatives, open all the Persons voted for, and Conviction of, Treason, Bribery, or other high Crimes and Misdemeanors.
-      </p>
+      <h1>About Me</h1>
+      <p>This is my personal website</p>
     </div>
     <br />
     <br />
