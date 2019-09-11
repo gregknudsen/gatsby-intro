9424245292b1a62b743ee1c6c84f557721cc23ec
Greg Knudsen <gknudsen@duqlight.com>
2019-09-11 10:11:47 -0400

    modify about page setup

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,13 @@
-import React from 'React';
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
 import Layout from '../components/layout';
 
-export default () => (
+const About = () => (
   <Layout>
-    <div>
-      <h1>About Me</h1>
-      <p>This is my personal website</p>
-    </div>
-    <br />
-    <br />
-    <br />
-    <Link to="/">&larr; Back to home</Link>
+    <h1>About Me</h1>
+    <p>This is my personal website.</p>
+    <Link to="/">&larr; back to home</Link>
   </Layout>
-)
+);
 
-
+export default About;
